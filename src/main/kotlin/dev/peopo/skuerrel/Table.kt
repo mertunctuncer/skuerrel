0bc3ad6eab31f328951a01e46b63a2adceec04d0
mertunctuncer <mertunctuncer@gmail.com>
2022-10-06 16:45:19 +0300

    Removed hikariCP dependency.

--- src/main/kotlin/dev/peopo/skuerrel/Table.kt
+++ src/main/kotlin/dev/peopo/skuerrel/Table.kt
@@ -1,7 +1,7 @@
 package dev.peopo.skuerrel
 
-import com.zaxxer.hikari.HikariDataSource
 import dev.peopo.skuerrel.annotation.Table
+import dev.peopo.skuerrel.connection.ConnectionProvider
 import dev.peopo.skuerrel.data.SQLPairList
 import dev.peopo.skuerrel.query.row.DeleteQuery
 import dev.peopo.skuerrel.query.row.InsertQuery
@@ -19,7 +19,7 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.findAnnotation
 
 
-class Table<T: Any> (val dataSource: HikariDataSource, kClass : KClass<out T>) {
+class Table<T: Any> (val dataSource: ConnectionProvider, kClass : KClass<out T>) {
 
 	internal val columns = TableSerializer.serialize(kClass)
 	internal val name : String = kClass.findAnnotation<Table>()?.name ?: throw IllegalArgumentException("Class must have @Table annotation")
@@ -30,7 +30,7 @@ class Table<T: Any> (val dataSource: HikariDataSource, kClass : KClass<out T>) {
 		var connection: Connection? = null
 		var result : ResultSet? = null
 		try {
-			connection = dataSource.connection
+			connection = dataSource.getConnection()
 			val meta = connection.metaData
 			result = meta.getTables(null, null, name, null)
 			val exist = result!!.next()
@@ -48,54 +48,54 @@ class Table<T: Any> (val dataSource: HikariDataSource, kClass : KClass<out T>) {
 
 	@Suppress("unused")
 	suspend fun create(@Suppress("unused_parameter")ifNotExist : Boolean = true) = withContext(Dispatchers.IO) {
-		CreateTableQuery(dataSource.connection, this@Table, true).execute()
+		CreateTableQuery(dataSource.getConnection(), this@Table, true).execute()
 	}
 
 	@Suppress("unused")
 	suspend fun drop() = withContext(Dispatchers.IO) {
-		DropTableQuery(dataSource.connection, this@Table).execute()
+		DropTableQuery(dataSource.getConnection(), this@Table).execute()
 	}
 
 	@Suppress("unused")
 	suspend fun insert(serializable: T) = withContext(Dispatchers.IO) {
-		InsertQuery(dataSource.connection, this@Table, DataSerializer.serialize(serializable)).execute()
+		InsertQuery(dataSource.getConnection(), this@Table, DataSerializer.serialize(serializable)).execute()
 	}
 
 	@Suppress("unused")
 	suspend fun update(serializable: T, where: SQLPairList) = withContext(Dispatchers.IO) {
-		UpdateQuery(dataSource.connection, this@Table, DataSerializer.serialize(serializable), where).execute()
+		UpdateQuery(dataSource.getConnection(), this@Table, DataSerializer.serialize(serializable), where).execute()
 	}
 
 	@Suppress("unused")
 	suspend fun update(set: SQLPairList, where: SQLPairList) = withContext(Dispatchers.IO) {
-		UpdateQuery(dataSource.connection, this@Table, set, where)
+		UpdateQuery(dataSource.getConnection(), this@Table, set, where)
 	}
 
 	@Suppress("unused")
 	suspend inline fun <reified K: Any>fetch(where: SQLPairList): List<K> = withContext(Dispatchers.IO){
-		val result = SelectQuery(dataSource.connection, this@Table, where).execute()
+		val result = SelectQuery(dataSource.getConnection(), this@Table, where).execute()
 		return@withContext result.map { DataSerializer.deserialize<K>(it) }
 	}
 
 	@Suppress("unused")
 	suspend inline fun <reified K: Any> fetch(where: String, values: List<Any?>) = withContext(Dispatchers.IO) {
-		val result = SelectQuery(dataSource.connection, this@Table, where, values).execute()
+		val result = SelectQuery(dataSource.getConnection(), this@Table, where, values).execute()
 		return@withContext result.map { DataSerializer.deserialize<K>(it) }
 	}
 
 	@Suppress("unused")
 	suspend inline fun <reified K: Any> fetchAll() = withContext(Dispatchers.IO) {
-		val result = SelectQuery(dataSource.connection, this@Table).execute()
+		val result = SelectQuery(dataSource.getConnection(), this@Table).execute()
 		return@withContext result.map { DataSerializer.deserialize<K>(it) }
 	}
 
 	@Suppress("unused")
 	suspend fun delete(where: SQLPairList) = withContext(Dispatchers.IO) {
-		DeleteQuery(dataSource.connection, this@Table, where).execute()
+		DeleteQuery(dataSource.getConnection(), this@Table, where).execute()
 	}
 
 	@Suppress("unused")
 	suspend fun deleteAll() = withContext(Dispatchers.IO) {
-		DeleteQuery(dataSource.connection, this@Table).execute()
+		DeleteQuery(dataSource.getConnection(), this@Table).execute()
 	}
 }
